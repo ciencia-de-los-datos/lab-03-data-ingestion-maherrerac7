d3e2d3b817eac6385f74a24884b7640d9fd24bc6
Maira Alejandra Herrera <160278900+maherrerac7@users.noreply.github.com>
2024-03-31 00:55:49 -0500

    primer intento

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,34 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import re
+
+def space_remove(text):
+    pattern = re.compile(r'\s+')
+    text = re.sub(pattern = pattern, repl= ' ',string = text)
+    text = re.sub(r'\.', repl= '',string = text)
+    return text
+
+
+def percentage_remove(text):
+    pattern = re.compile(r'(\d+),(\d+)\s%')
+    text = re.sub(pattern=pattern, repl=r'\1.\2', string=text)
+    text = float(text)
+    return text
 
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_fwf("clusters_report.txt", skiprows=4, header = None)
+    
+    df.columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
+    
+    df['principales_palabras_clave']=df.ffill().groupby('cluster')['principales_palabras_clave'].transform(lambda x: ' '.join(x))
+    
+    df=df.dropna().reset_index(drop=True)
+    
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(space_remove)
+    
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].apply(percentage_remove)
 
     return df
